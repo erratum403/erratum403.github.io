99c4d8ea5ac4942078bc6d0f846960089944deb1
definexzero <eden.hila@campus.technion.ac.il>
2025-09-26 20:18:04 +0300

    meta data and icon change

--- src/site.config.ts
+++ src/site.config.ts
@@ -14,7 +14,7 @@ export const siteConfig: SiteConfig = {
 		},
 	},
 	// Used as the default description meta property and webmanifest description
-	description: "An opinionated starter theme for Astro",
+	description: "writing space of my own",
 	// HTML lang property, found in src/layouts/Base.astro L:18 & astro.config.ts L:48
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
@@ -25,7 +25,7 @@ export const siteConfig: SiteConfig = {
 		- The link value found in src/components/layout/Header.astro L:35
 		- In the footer found in src/components/layout/Footer.astro L:12
 	*/
-	title: "Astro Cactus",
+	title: "define x zero",
 	// ! Please remember to replace the following site property with your own domain, used in astro.config.ts
 	url: "https://erratum403.github.io",
 };
